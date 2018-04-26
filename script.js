0918b9b60ebe1a9fe328ee6123f5543d4e9bda82
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-26 16:46:07 +0000

    Init

---
+++ script.js
@@ -0,0 +1,6 @@
+// for (var i = 0; i < document.frmEva.cnt.value; i++)
+//   document.querySelector(`input[name='ra_scale${i}'][value='4']`).checked = true;
+// document.frmEva.submit()
+
+// minify!!
+var a=document,b=a.frmEva;for(var i=0;i<b.cnt.value;i++)a.querySelector("input[name='ra_scale"+i+"'][value='4']").checked=!0;b.submit();
